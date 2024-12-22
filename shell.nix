47d7c9ee66fcb36a46c76c4dfd1452fd0bf258aa
Cenk Uluisik <cenk.uluisik@googlemail.com>
2024-12-22 22:25:54 +0100

    Add nix-shell

---
+++ shell.nix
@@ -0,0 +1,28 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell rec {
+  buildInputs = with pkgs; [
+    # Rust Stuff
+    rustc
+    cargo
+    rust-analyzer
+    rustfmt
+    
+    #iced-rs stuff
+    expat
+    fontconfig
+    freetype
+    freetype.dev
+    libGL
+    pkg-config
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXi
+    xorg.libXrandr
+    wayland
+    libxkbcommon
+  ];
+
+  LD_LIBRARY_PATH =
+    builtins.foldl' (a: b: "${a}:${b}/lib") "${pkgs.vulkan-loader}/lib" buildInputs;
+}
