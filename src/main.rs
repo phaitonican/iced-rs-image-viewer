f44a926a33a59b827e894c4efe6d1636d82d9ab5
Cenk Uluisik <cenk.uluisik@googlemail.com>
2024-12-23 04:45:37 +0100

    Cleanup some stuff

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::path::PathBuf;
 
-use iced::widget::{column, pick_list, slider, text, text_input, Button, Row};
+use iced::widget::{column, pick_list, scrollable, slider, text, Button, Column};
 use iced::widget::{row, Image};
 use iced::{Element, Fill, Theme};
 use rfd::FileDialog;
@@ -16,21 +16,27 @@ pub fn main() -> iced::Result {
     .run()
 }
 
-#[derive(Default)]
 struct ImageViewer {
     theme: Theme,
-    input_value: String,
-    folder_paths: Option<Vec<PathBuf>>,
     image_paths: Option<Vec<PathBuf>>,
     image_size: f32,
+}
+
+impl Default for ImageViewer {
+    fn default() -> Self {
+        Self {
+            theme: Theme::default(),
+            image_paths: Some(Vec::new()),
+            image_size: 25.0,
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
 enum Message {
     ThemeChanged(Theme),
-    InputChanged(String),
-    SelectFolder,
     ImageSizeChanged(f32),
+    SelectFolder,
 }
 
 impl ImageViewer {
@@ -39,32 +45,27 @@ impl ImageViewer {
             Message::ThemeChanged(theme) => {
                 self.theme = theme;
             }
-            Message::InputChanged(value) => self.input_value = value,
+            Message::ImageSizeChanged(value) => {
+                self.image_size = value;
+            }
             Message::SelectFolder => {
-                self.folder_paths = FileDialog::new()
+                let folder_paths = FileDialog::new()
                     //.add_filter("text", &["txt", "rs"])
                     //.add_filter("rust", &["rs", "toml"])
                     //.set_directory("/")
                     .pick_folders();
 
                 // return if nothing selected
-                if self.folder_paths.is_none() {
+                if folder_paths.is_none() {
                     return;
                 }
 
+                //remove old image_paths (to avoid duplicate images)
                 self.image_paths = Some(Vec::new());
-
-                //update textbox
-                let mut folder_paths_string = "".to_owned();
-                for folder_path in self.folder_paths.as_ref().unwrap() {
-                    folder_paths_string.push_str(", ");
-                    folder_paths_string
-                        .push_str(folder_path.as_os_str().to_str().unwrap_or_default());
-
-                    //get image file paths
+                //update image paths
+                for folder_path in folder_paths.as_ref().unwrap().iter() {
                     let paths = fs::read_dir(folder_path).unwrap();
-
-                    for path in paths {
+                    for path in paths.into_iter() {
                         if path
                             .as_ref()
                             .unwrap()
@@ -80,45 +81,38 @@ impl ImageViewer {
                         }
                     }
                 }
-                self.input_value = folder_paths_string;
-            }
-            Message::ImageSizeChanged(value) => {
-                self.image_size = value;
             }
         }
     }
 
     fn view(&self) -> Element<Message> {
-        let text_input =
-            text_input("path to images...", &self.input_value).on_input(Message::InputChanged);
-
         let choose_theme = row![
             text("Theme:"),
-            pick_list(Theme::ALL, Some(&self.theme), Message::ThemeChanged).width(Fill),
+            pick_list(Theme::ALL, Some(&self.theme), Message::ThemeChanged),
         ];
 
-        let button = Button::new("Select Folder").on_press(Message::SelectFolder);
+        let select_folder = Button::new("Select Folder").on_press(Message::SelectFolder);
 
-        let slider = slider(0.0..=100.0, self.image_size, Message::ImageSizeChanged);
+        let image_size = row![
+            text("Zoom:"),
+            slider(0.0..=100.0, self.image_size, Message::ImageSizeChanged)
+        ];
 
-        let mut image_elements = Vec::new();
-
+        // create images
+        let mut images = Vec::new();
         if self.image_paths.is_some() {
-            for image_path in self.image_paths.as_ref().unwrap() {
-                let image_element = Element::new(
-                    Image::new(image_path)
-                        .height(self.image_size.powf(2.0))
-                        .width(self.image_size.powf(2.0)),
-                );
-                image_elements.push(image_element);
+            for image_path in self.image_paths.as_ref().unwrap().iter() {
+                let image = Image::new(image_path)
+                    //.height(self.image_size.powf(2.0))
+                    .width(self.image_size.powf(2.0))
+                    .into();
+                images.push(image);
             }
         }
+        let image_element = scrollable(Column::from_vec(images)).width(Fill);
 
-        let content = column![
-            row![choose_theme, text_input, button],
-            row![slider],
-            Row::from_vec(image_elements)
-        ];
+        // create content
+        let content = column![row![choose_theme, image_size, select_folder], image_element,];
 
         content.into()
     }
