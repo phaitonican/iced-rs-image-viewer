2a89131b6b9340f7c159485577a6f0b8e67ba0b2
Cenk Uluisik <cenk.uluisik@googlemail.com>
2024-12-23 23:02:08 +0100

    Fixing the reload of images on every interaction

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use iced::widget::{self, column, pick_list, scrollable, text, Button, Column, Space};
+use iced::widget::image::Handle;
+use iced::widget::{column, pick_list, scrollable, text, Button, Column};
 use iced::widget::{row, Image};
 use iced::{Element, Fill, Theme};
 use image::ImageReader;
@@ -20,6 +21,7 @@ struct ImageViewer {
     theme: Theme,
     image_paths: Option<Vec<PathBuf>>,
     thumbnail_size: i32,
+    thumbnail_handles: Option<Vec<Handle>>,
 }
 
 impl Default for ImageViewer {
@@ -28,6 +30,7 @@ impl Default for ImageViewer {
             theme: Theme::default(),
             image_paths: Some(Vec::new()),
             thumbnail_size: 200,
+            thumbnail_handles: Some(Vec::new()),
         }
     }
 }
@@ -47,6 +50,7 @@ impl ImageViewer {
             }
             Message::ThumbnailSizeChanged(value) => {
                 self.thumbnail_size += value;
+                //self.recreate_images();
             }
             Message::SelectFolder => {
                 let folder_paths = FileDialog::new()
@@ -56,6 +60,7 @@ impl ImageViewer {
                     .pick_folders();
 
                 self.get_image_paths(folder_paths);
+                self.recreate_images();
             }
         }
     }
@@ -69,8 +74,8 @@ impl ImageViewer {
         self.image_paths = Some(Vec::new());
         //update image paths
         for folder_path in folder_paths.as_ref().unwrap().iter() {
-            let paths = fs::read_dir(folder_path).unwrap();
-            for path in paths.into_iter() {
+            let paths_inside_folder = fs::read_dir(folder_path).unwrap();
+            for path in paths_inside_folder.into_iter() {
                 if path
                     .as_ref()
                     .unwrap()
@@ -88,27 +93,22 @@ impl ImageViewer {
         }
     }
 
-    fn create_image_widget(&self) -> Element<Message> {
+    fn recreate_images(&mut self) {
         // create images
-        let mut processed_images = Vec::new();
+        self.thumbnail_handles = Some(Vec::new());
         if self.image_paths.is_some() {
             for image_path in self.image_paths.as_ref().unwrap().iter() {
                 let image = ImageReader::open(&image_path).unwrap().decode().unwrap();
-                let thumbnail = image
-                    .thumbnail(self.thumbnail_size as u32, self.thumbnail_size as u32)
-                    .into_rgba8();
-                let thumbnail_widget = widget::image::Handle::from_rgba(
-                    thumbnail.width(),
-                    thumbnail.height(),
-                    thumbnail.into_raw(),
-                );
+                let thumbnail = image.thumbnail(200, 200).into_rgba8();
+                let thumbnail_handle =
+                    Handle::from_rgba(thumbnail.width(), thumbnail.height(), thumbnail.into_raw());
 
-                let processed_image = Image::new(thumbnail_widget).into();
-                processed_images.push(processed_image);
+                self.thumbnail_handles
+                    .as_mut()
+                    .unwrap()
+                    .push(thumbnail_handle);
             }
         }
-        let processed_images_element = scrollable(Column::from_vec(processed_images)).width(Fill);
-        processed_images_element.into()
     }
 
     fn view(&self) -> Element<Message> {
@@ -121,8 +121,12 @@ impl ImageViewer {
         let zoom_in = Button::new("+").on_press(Message::ThumbnailSizeChanged(50));
         let zoom_out = Button::new("-").on_press(Message::ThumbnailSizeChanged(-50));
 
-        // create images
-        let processed_images_element = self.create_image_widget();
+        let mut image_elements = Vec::new();
+        for thumbnail_handle in self.thumbnail_handles.as_ref().unwrap() {
+            let image_element = Image::new(thumbnail_handle).width(self.thumbnail_size as f32);
+            image_elements.push(image_element.into());
+        }
+        let processed_images_element = scrollable(Column::from_vec(image_elements)).width(Fill);
 
         // create content
         let content = column![
